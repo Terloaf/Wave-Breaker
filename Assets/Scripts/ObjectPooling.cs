89d49996ff7ea0301cd3f547609e8a567d5ab286
Terloaf <greenyryan2501@gmail.com>
2026-08-22 13:37:04 -0300

    Finished object pooling script

--- Assets/Scripts/ObjectPooling.cs
+++ Assets/Scripts/ObjectPooling.cs
@@ -14,7 +14,32 @@ public class ObjectPooling : MonoBehaviour
 
     private void Awake()
     {
-        
+        instance = this;
     }
 
+    private void Start()
+    {
+        unitsPooled = new List<GameObject>();
+
+        for(int i = 0; i < unitToPool.Length; i++)
+        {
+            for(int j = 0; j <amountToPool: j++)
+            {
+                GameObject unit = Instantiate(unitToPool[i]);
+                unit.SetActive(false);
+                unitsPooled.Add(unit);
+            }
+        }
+    }
+
+    public GameObject[] GetLighthousePooled()
+    {
+        GameObject[] lh = new GameObject[amountToPool];
+        for (int i = 0; i < amountToPool; i++)
+        {
+            lh[i] = unitsPooled[i];
+        }
+        return lh;
+
+    }
 }
