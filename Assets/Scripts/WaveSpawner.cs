55e1ef2d472ef86feb25e3ac9dd7ffe17f7e1ad0
Terloaf <greenyryan2501@gmail.com>
2026-08-23 01:06:01 -0300

    updated wave spawner

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -1,26 +1,40 @@
 using UnityEngine;
+using System.Collections;
 
 public class WaveSpawner : MonoBehaviour
 {
     public Transform enemyPrefab;
+    public Transform spawnPoint;
 
     public float timeBetweenWaves = 5f;
-    private float countdown = 2f; 
-
+    private float countdown = 2f;
+    private int waveIndex = 1;
     private void Update()
     {
         if (countdown <= 0f)
         {
-            SpawnWave();
+            StartCoroutine(SpawnWave());
             countdown = timeBetweenWaves;
         }
 
         countdown -= Time.deltaTime;
     }
 
-    void SpawnWave()
+    IEnumerator SpawnWave()
     {
-        Debug.Log("Wave coming");
+        waveIndex++;
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.2f);
+        }
+        
+
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
     }
 
 }
